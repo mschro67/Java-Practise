c31317935702eaac5bad1a9d4d6e10a849dd4123
Maxi <maximilian.s107@evt.schule.koeln>
2025-11-27 12:50:45 +0100

    Delete src directory

--- src/Chapter2/Ex7.java
+++
@@ -1,9 +0,0 @@
-//by mschro67
-
-package Chapter2;
-
-public class Ex7{
-    public static void main(String[] args){
-        System.out.println("Endlich ist es soweit! Mein erstes Programm läuft...");
-    }
-}
